a61345e4883ead9445757188bcd4b5f9dd3c4a3e
Jeffrey Merkey <jeffmerkey@gmail.com>
2026-01-04 20:26:25 -0800

    add xscreensaver v5.42
    Signed-off-by: Jeffrey Merkey <jeffmerkey@gmail.com>

--- utils/version.h
+++ utils/version.h
@@ -1,2 +1,2 @@
 static const char screensaver_id[] =
-	"@(#)xscreensaver 5.40 (12-Aug-2018), by Jamie Zawinski (jwz@jwz.org)";
+	"@(#)xscreensaver 5.42 (28-Dec-2018), by Jamie Zawinski (jwz@jwz.org)";
